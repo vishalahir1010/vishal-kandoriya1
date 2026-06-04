66817ea90c181f7aefc22cfa0f4bc2fa9b292a6a
vishalahir1010 <ahirv2641@gmail.com>
2026-06-04 11:03:40 +0530

    update

--- assesment/script.js
+++ assesment/script.js
@@ -1,100 +1,36 @@
 const form = document.getElementById("linkForm");
-const titleInput = document.getElementById("title");
-const urlInput = document.getElementById("url");
+const title = document.getElementById("title");
+const url = document.getElementById("url");
 const preview = document.getElementById("preview");
 const error = document.getElementById("error");
-const badge = document.getElementById("badge");
 const themeBtn = document.getElementById("themeBtn");
 
-let links = JSON.parse(localStorage.getItem("links")) || [];
-
-// Verified Badge
-const followers = 1500;
-
-if (followers >= 1000) {
-    badge.textContent = "✔ Verified";
-}
-
-// Render Links
-function renderLinks() {
-
-    preview.innerHTML = "";
-
-    links.forEach((link, index) => {
-
-        preview.innerHTML += `
-            <div class="link-card">
-                <a href="${link.url}" target="_blank">
-                    ${link.title}
-                </a>
-
-                <button onclick="deleteLink(${index})">
-                    Remove
-                </button>
-            </div>
-        `;
-    });
-}
-
-// Save Data
-function saveLinks() {
-    localStorage.setItem("links", JSON.stringify(links));
-}
-
-// Add Link
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function(e){
 
     e.preventDefault();
 
-    const title = titleInput.value.trim();
-    const url = urlInput.value.trim();
-
-    const regex = /^https:\/\/.+/;
-
-    if (!regex.test(url)) {
-        error.textContent =
-        "URL must start with https://";
+    if(!url.value.startsWith("http")){
+        error.innerText = "Please enter a valid URL";
         return;
     }
 
-    error.textContent = "";
+    error.innerText = "";
 
-    links.push({
-        title,
-        url
-    });
+    const card = document.createElement("div");
+    card.classList.add("link-card");
 
-    saveLinks();
-    renderLinks();
+    card.innerHTML = `
+        <a href="${url.value}" target="_blank">
+            🔗 ${title.value}
+        </a>
+    `;
 
-    form.reset();
+    preview.appendChild(card);
+
+    title.value = "";
+    url.value = "";
 });
 
-// Delete Link
-function deleteLink(index) {
-
-    links.splice(index, 1);
-
-    saveLinks();
-    renderLinks();
-}
-
-// Theme Toggle
-themeBtn.addEventListener("click", () => {
-
+themeBtn.addEventListener("click", function(){
     document.body.classList.toggle("dark");
-
-    let mode =
-    document.body.classList.contains("dark")
-    ? "dark"
-    : "light";
-
-    localStorage.setItem("theme", mode);
 });
-
-// Load Theme
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-}
-
-renderLinks();
